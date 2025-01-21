7366a793f2f07b791a28c4eca413d69adc19f0a4
devsafix <devsafix@gmail.com>
2025-01-21 11:05:04 +0600

    Add onboard page and validate user

---
+++ src/app/onboard/page.js
@@ -0,0 +1,11 @@
+
+
+const OnBoard = () => {
+    return (
+        <div>
+            This is the onboarding page.
+        </div>
+    );
+};
+
+export default OnBoard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div></div>;
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+  const user = await currentUser();
+
+  const profileInfo = null;
+
+  if(user && !profileInfo?._id) {
+    redirect('/onboard');
+  }
+
+  return (
+    <div>
+      {user ? (
+        <div>Welcome, {user.firstName}!</div>
+      ) : (
+        <div>Please log in to access your account.</div>
+      )}
+    </div>
+  );
 }
