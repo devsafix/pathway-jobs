e5c434ee4c6612a647db40c609da23693ee38c69
devsafix <devsafix@gmail.com>
2025-01-23 09:00:58 +0600

    Loading update

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,9 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center space-x-2">
-      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-600"></div>
-      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-600"></div>
-      <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-600"></div>
+    <div className="flex items-center justify-center space-x-2 py-40">
+      <div className="w-8 h-8 rounded-full animate-pulse bg-black"></div>
+      <div className="w-8 h-8 rounded-full animate-pulse bg-black"></div>
+      <div className="w-8 h-8 rounded-full animate-pulse bg-black"></div>
     </div>
   );
 };
