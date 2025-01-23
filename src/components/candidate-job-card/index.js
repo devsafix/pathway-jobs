b40a3fd5c1184a2148884fd44c07df782779a225
devsafix <devsafix@gmail.com>
2025-01-23 10:44:33 +0600

    drawer added

--- src/components/candidate-job-card/index.js
+++ src/components/candidate-job-card/index.js
@@ -7,22 +7,52 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
 import { Button } from "../ui/button";
 
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useState } from "react";
+
 const CandidateJobCard = ({ job }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
-    <Card>
-      <CardHeader>
-        {/* add one icon also */}
-        <CardTitle>{job.title}</CardTitle>
-        <CardDescription>{job.description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>{job.location}</p>
-        <p>{job.name}</p>
-        <Button className="mt-5">View Details</Button>
-      </CardContent>
-    </Card>
+    <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+      <Card>
+        <CardHeader>
+          {/* add one icon also */}
+          <CardTitle>{job.title}</CardTitle>
+          <CardDescription>{job.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{job.location}</p>
+          <p>{job.name}</p>
+          <Button onClick={() => setIsDrawerOpen(true)} className="mt-5">
+            View Details
+          </Button>
+        </CardContent>
+      </Card>
+      <DrawerContent className="p-5 h-60">
+        <DrawerHeader>
+          <DrawerTitle className="text-2xl font-bold flex justify-between items-center">
+            {job.title}
+            <div className="flex items-center gap-2">
+              <Button>Apply</Button>
+              <Button onClick={() => setIsDrawerOpen(false)}>Close</Button>
+            </div>
+          </DrawerTitle>
+          <DrawerDescription>{job.description}</DrawerDescription>
+        </DrawerHeader>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
